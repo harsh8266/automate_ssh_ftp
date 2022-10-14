dbf17ab296816de2d3a1fed03461e28b0d786bea
Harsh8266 <58392509+harsh8266@users.noreply.github.com>
2022-10-14 21:07:30 +0530

    Update1

--- ssh_ftp.sh
+++ ssh_ftp.sh
@@ -1,30 +1,59 @@
 #!/bin/bash
 
+echo " run with sudo -i "
+sleep 6
+if [ $USER == 'root' ]; then
 echo "installing ssh"
 sleep 1
-sudo apt install ssh -y
+apt install -y ssh 
 sleep 2
 echo "enable ssh"
 sleep 1
-sudo systemctl enable ssh
+systemctl enable ssh
 
 echo "installing vsftpd"
 sleep 1
 
-sudo apt install vsftpd -y
+apt install -y vsftpd 
 sleep 3
 
 echo "Create a new FTP user "
 read -p "Enter a username:- " USERNAME
 read -p "Enter a Password:- " PASSWORD
 
-sudo useradd -m $USERNAME -p $PASSWORD
+useradd -m $USERNAME -p $PASSWORD
 
 sleep 5
 
-sudo ufw allow 20/tcp
-sleep 1
-sudo ufw allow 21/tcp
-sleep 1
 
-echo "Done"
+systemctl enable vsftpd
+apt install ufw
+ufw enable
+ufw allow 20,21,990/tcp
+echo "$USERNAME" | sudo tee -a /etc/vsftpd.userlist
+cd /home/$USERNAME
+mkdir ftp
+mkdir ftp/upload
+sudo chown nobody:nogroup /home/user1/ftp 
+sudo chmod a-w /home/user1/ftp 
+sudo chown user1:user1 /home/user1/ftp/upload
+echo "write_enable=YES" | sudo tee -a /etc/vsftpd.conf
+
+echo "chroot_local_user=YES" | sudo tee -a /etc/vsftpd.conf
+
+echo "allowed_writeable_chroot=YES" | sudo tee -a /etc/vsftpd.conf
+
+
+echo "userlist_enable=YES" | sudo tee -a /etc/vsftpd.conf
+echo "userlist_file=/etc/vsftpd.userlist" | sudo tee -a /etc/vsftpd.conf
+echo "userlist_deny=NO" | sudo tee -a /etc/vsftpd.conf
+
+sudo service vsftpd restart
+
+echo "Done!!!!!!!!!!!!!!!!!"
+
+else
+
+echo "run with sudo -i "
+
+fi
