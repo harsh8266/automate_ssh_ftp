56605c728129395a8eb5c91a52ec2d3c21ee6100
Harsh8266 <58392509+harsh8266@users.noreply.github.com>
2022-10-15 19:29:46 +0530

    F2

--- ssh_ftp.sh
+++ ssh_ftp.sh
@@ -2,7 +2,7 @@
 
 echo "installing ssh"
 sleep 1
-sudo apt install -y ssh 
+sudo apt install -y ssh
 sleep 2
 echo "enable ssh"
 sleep 1
@@ -11,21 +11,21 @@ sudo systemctl enable ssh
 
 sudo apt install ufw
 sudo ufw enable
-sudo ufw allow 20,21/tcp
-sudo ufw allow 5000:10000/tcp 
+sudo ufw allow 20,21,22/tcp
+sudo ufw allow 5000:10000/tcp
 
-read -p "Enter a username:- " USERNAME
-read -p "Enter a Password:- " PASSWORD
 
-sudo useradd -m $USERNAME -p $PASSWORD
+echo "Enter password:- superman"
+sleep 4
+sudo adduser shark
 
-echo "Enter the same password below:-"
-sudo passwd $USERNAME
+echo "AllowUsers shark" | sudo tee -a /etc/ssh/sshd_config
 echo "installing proftpd"
 sleep 1
 
 
-sudo apt install proftpd 
+sudo apt install proftpd
+sudo systemctl restart ssh
 sleep 3
 
 
